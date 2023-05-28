8d20f68bc60ecda0812485582fc4c6a9fec71eea
Vladimir Istomin <56888502+JamesBomzh@users.noreply.github.com>
2023-05-28 14:13:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main() {
 	cout << "What is your name?\n"; //Asking for a user's name
 	string name; //var with a username
 	cin >> name; //binding a name to the var
-	cout << "Hello, " << name << endl; //Greeting the user
+	cout << "Hello, " << name << endl; //Greeting the user - изменение для конфликта
 	return 0;
 }
