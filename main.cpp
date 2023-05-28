c18f03d1e0f941fee8c196d9d3caad2edf9a1b04
James Bomzh <Bomzh4CB@yandex.ru>
2023-05-28 13:58:45 +0300

    1st new branch commit

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@
 using namespace std;
 
 int main() {
-	cout << "What is your name?\n";
+	cout << "What is your name?\n"; //Asking for a user's name
 	string name;
 	cin >> name;
-	cout << "Hello, " << name << endl;
+	cout << "Hello, " << name << endl; //Greeting the user
 	return 0;
 }
